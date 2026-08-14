c8d75abc85fb310fcb6d4f178d5399a3210b0e9d
kingfabricio59-sketch <kingfabricio59@gmail.com>
2026-08-14 12:48:09 -0300

    Delete page.tsx

--- page.tsx
+++
@@ -1,8 +0,0 @@
-import CherosaApp from "./cherosa-app";
-
-export const dynamic = "force-dynamic";
-
-export default function Home() {
-  const adminName = process.env.ADMIN_NAME?.trim() || "Administración Cherosa";
-  return <CherosaApp adminName={adminName} />;
-}
